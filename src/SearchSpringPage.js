b79884b968f0855af20492d16f315121b49216d2
pavlodidushko <pavlodidushko@gmail.com>
2025-06-10 16:30:22 +0300

    remove array length 1 from searchspring

--- src/SearchSpringPage.js
+++ src/SearchSpringPage.js
@@ -60,6 +60,9 @@ const SearchSpringPage = ({ query, selectedFacets, setSelectedFacets }) => {
       setSearchSpringPaginationInfo(data.pagination || {});
       data.facets = data.facets.filter(facet => facet.field !== "obsolete");
       data.facets = mapAttributesFacet(data.facets);
+
+      data.facets = data.facets.filter(facet => facet.facet_values.length > 1);
+
       setSearchSpringFacets(data.facets || []);
     } catch (error) {
       console.error("Error fetching SearchSpring results:", error);
